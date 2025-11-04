e11bf1742276bd3a99d642fb5e2f6707dd8c823a
Yizhe Sun <syzroy@gmail.com>
2025-11-04 03:34:42 +0000

    add tauri

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
+const host = process.env.TAURI_DEV_HOST;
+
 const config = defineConfig({
   plugins: [
     nitroV2Plugin(),
@@ -20,6 +22,28 @@ const config = defineConfig({
       },
     }),
   ],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 })
 
 export default config
